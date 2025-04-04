f14b0c8093cf134c8ed6365ca03af92b7c7fe22f
osman1126-sid <137859898+osman1126-sid@users.noreply.github.com>
2025-04-04 15:40:57 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+import PyPDF2
 import streamlit as st
 from google import genai
 api_key = st.secrets["GENAI_API_KEY"]
@@ -6,15 +7,19 @@ counter=1
 st.set_page_config(page_title="Gemini Chatbot", layout="centered")
 st.title("Gemini Chatbot")
 st.write("Ask me anything based on the given data. If I don't know the answer, I'll reply with 'Not known'.")
+uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
 
-text="""FARHAN SIDDIQUE Dept: Electronics & Communication Engineering 
-    Year of Graduation: 2019 Degree: B-TechCollege: Durgapur Institute of Advanced Technology & Management
-    .E-Mail:farhansiddique027@gmail.com Phone No:09064654440(M)/8927141595 OBJECTIVE:Seeking a position in well renowned sector
-    in well renowned sector where I can utilize my skills & abilities achieve professional growth while being innovative and 
-    flexible exploring my internal smartness VISION: My vision is to earn success from this lovely world and thereby to establish
-    myself as an honest and ideal perso. Strength: My strength is positive attitude and self-confidence. HOBBIES Playing Cricket. 
-    TECHNICAL EXPOSURE: LANGUAGE:–Basics of CORE JAVA,PYTHON. PROFICIENT IN COMPUTER OPERATING,
-    INTERNET SURFING, MS OFFICE, MS WORD , MS EXCEL ETC. Sem-1 6.89 Sem-2 6.00"""
+# Check if a file is uploaded
+if uploaded_file is not None:
+    # Read the PDF content
+    reader = PyPDF2.PdfReader(uploaded_file)
+    st.success("PDF Uploaded Successfully!")
+    for page in reader.pages:
+        texxt = page.extract_text()
+        if texxt:
+            text += texxt + "\n"
+
+    st.success("Text extracted and stored in 'doc' variable ✅")
 while counter!=0:
     user_prompt = st.text_input("💬 You:", "")
     if user_prompt=="exit" :
