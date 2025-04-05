0badd1d5cae8ed60b2cd698787a80eebcef375aa
osman1126-sid <137859898+osman1126-sid@users.noreply.github.com>
2025-04-05 22:34:01 +0530

    Update code.py

--- code.py
+++ code.py
@@ -33,6 +33,6 @@ while counter!=0:
     response=client.models.generate_content(
     model="gemini-2.0-flash", contents=[{"role": "user", "parts": [{"text": prompt}]}])
    
-    print(f"**Gemini:** {response.text}")
+    st.markdown(f"**Gemini:** {response.text}")
 
 # Exit button
